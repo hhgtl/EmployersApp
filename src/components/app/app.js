06502912e3c17ae29eb80f673906bf5256814a35
Petro <petrosahal66@gmail.com>
2024-05-28 18:20:17 +0300

    Ready sorting by search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,31 +5,32 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
-import {useEffect, useState } from 'react';
+import {useMemo, useState } from 'react';
 
 function App() {
   let [state, setState] = useState({
     employees: [{id: 0, fullName: 'John Smith', salary: 900, increaseSalary: false}, {id: 1, fullName: 'Alex Rich', salary: 2000, increaseSalary: true}, {id: 2, fullName: 'Elon Musk', salary: 1000, increaseSalary: false}],
     sortEmployees: {sortType: '', sortedEmployees: []},
-    newEmployee: {fullName: '', salary: ''}
+    newEmployee: {fullName: '', salary: ''},
+    searchValue: '',
   })
-  const sortEmployees = () => {
+  const sortEmployees = useMemo( () => {
     switch(state.sortEmployees.sortType) {
       case 'increase':
-        setState({...state, sortEmployees: {...state.sortEmployees, sortedEmployees: state.employees.filter( emp => emp.increaseSalary)}})
-        break
+        return [...state.employees].filter( emp => emp.increaseSalary)
       case 'moreThan1000':
-        setState({...state, sortEmployees: {...state.sortEmployees, sortedEmployees: state.employees.filter( emp => emp.salary >= 1000)}})
-        break
+        return [...state.employees].filter( emp => emp.salary >= 1000)
       default: 
-        setState({...state, sortEmployees: {...state.sortEmployees, sortedEmployees: [...state.employees]}})
-        break
+        return [...state.employees]
     }
+  }, [state.employees, state.sortEmployees.sortType]) // Сортування робітників за фільтром
+  const sortedAndSearchedEmployees = useMemo( () => {
+    return sortEmployees.filter( emp => emp.fullName.toLocaleLowerCase().includes(state.searchValue.toLocaleLowerCase()))
+  }, [state.searchValue, sortEmployees]) // Сортування за пошуком
+  const setSearchValue = (e) => { // Редагувати пошукової строки
+    setState({...state, searchValue: e.target.value})
   }
-  useEffect(() => {
-    sortEmployees();
-  });
-  const selectTypeSort = (sortType) => {
+  const selectTypeSort = (sortType) => { // Зміна типу сортировки
     setState({...state, sortEmployees: {...state.sortEmployees, sortType: sortType}})
   }
   const setSalary = (id, salaryValue) => { // Редагувати заробітну плату робітника
@@ -58,18 +59,18 @@ function App() {
     })})
   }
   const totalEmployees = state.employees.length; // Загальна кількість працівників
-  let totalIncrease = 0; // Кількість працівників, які получать премію
-  state.employees.forEach( emp => emp.increaseSalary ? totalIncrease += 1 : null)
+  let totalIncrease = 0; // Рахунок працівників з премією
+  state.employees.forEach( emp => emp.increaseSalary ? totalIncrease += 1 : null) // Кількість працівників, які получать премію
   return (
     <div className="app">
         <AppInfo totalEmployees={totalEmployees} totalIncrease={totalIncrease}/>
 
         <div className="search-panel">
-            <SearchPanel/>
+            <SearchPanel searchValue={state.searchValue} setSearchValue={setSearchValue}/>
             <AppFilter selectTypeSort={selectTypeSort}/>
         </div>
         
-        <EmployeesList employees={state.sortEmployees.sortedEmployees} setSalary={setSalary} removeEmployee={removeEmployee} toggleIncrease={toggleIncrease}/>
+        <EmployeesList employees={sortedAndSearchedEmployees} setSalary={setSalary} removeEmployee={removeEmployee} toggleIncrease={toggleIncrease}/>
         <EmployeesAddForm newEmployee={state.newEmployee} addEmployee={addEmployee} changeEmployeeNameOnForm={changeEmployeeNameOnForm} changeEmployeeSalaryOnForm={changeEmployeeSalaryOnForm}/>
     </div>
   );
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -3,6 +3,7 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employees-list.css';
 
 const EmployeesList = ({employees, setSalary, removeEmployee, toggleIncrease}) => {
+    debugger
     let employeesList = employees.map(item => <EmployeesListItem key={item.id} id={item.id} fullName={item.fullName} salary={item.salary} increaseSalary={item.increaseSalary} setSalary={setSalary} removeEmployee={removeEmployee} toggleIncrease={toggleIncrease}/>)
     return (
         <ul className="app-list list-group">
